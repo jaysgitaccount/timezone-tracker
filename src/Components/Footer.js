4de02b26d9a0e325c425be2028d8d29183a65184
Jay <jay.webp@gmail.com>
2023-01-05 13:26:14 +1100

    Rearrange text in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
     return (
         <footer>
-            <p>Designed & coded by Jay</p>
             <p>Powered by <a href="https://worldtimeapi.org/" target="_blank">World Time API</a></p>
+            <p>Designed & coded by Jay</p>
         </footer>
     )
 }
