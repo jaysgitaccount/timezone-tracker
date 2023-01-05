b93f981d9b27eb7c13d8b4b6ddbedc3bfa3fa76e
Jay <jay.webp@gmail.com>
2023-01-05 13:26:58 +1100

    Implement basic clock UI

--- src/Components/DisplayComponents/Clock.js
+++ src/Components/DisplayComponents/Clock.js
@@ -1,7 +1,11 @@
+import ClockHand from "./ClockHand";
+
 function Clock(props) {
     return (
         <div className="Clock">
-
+            <ClockHand classes={'minute'} />
+            <ClockHand classes={'hour'} />
+            <div className="pivot center-absolute"></div>
         </div>
     )
 }
---
+++ src/Components/DisplayComponents/ClockHand.js
@@ -0,0 +1,10 @@
+function ClockHand(props) {
+    return (
+        <div className={`ClockHand center-absolute ${props.classes}`}>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default ClockHand;
