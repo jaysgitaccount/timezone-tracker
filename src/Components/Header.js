1b5e3d7795a57ed8ddc0cc51e907022f5fb67e09
Jay <jay.webp@gmail.com>
2023-01-10 16:36:19 +1100

    Show updated header text

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,10 +8,10 @@ function Header() {
             </h1>
             <div>
                 <p>
-                    Have you ever found yourself thinking, "I wish I knew what time it was in (insert country here)"? 
+                    Have you ever found yourself thinking, <i>"I wish I knew what time it was in (insert country here), so I know when I can (insert time-dependent activity here)"</i>? Well, now you don't have to! Using this timezone tracker, you can add and compare timezones to your heart's content.
                 </p>
                 <p>
-                    Well, now you don't have to! With this webpage, you can add and compare timezones to your heart's content.
+                    To get started, simply select a timezone from the list below.
                 </p>
             </div>
         </header>
