76ca4b19574eb2176d8423e23719ebeacd11f33b
Jay <jay.webp@gmail.com>
2023-01-10 17:18:01 +1100

    Display underlines for custom time result and location

--- src/Components/DisplayComponents/CustomTimeInput.js
+++ src/Components/DisplayComponents/CustomTimeInput.js
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 
 function CustomTimeInput(props) {
     const [value, setValue] = useState(props.data);
-    const [label, setLabel] = useState(['Pick a time', ''])
+    const [label, setLabel] = useState(['Pick a time','', ''])
 
     useEffect(() => {
         if (Object.keys(props.data).length <= 1) {
             // If no time conversion, use initial values
             setValue(props.data.initialValue)
-            setLabel(['Pick a time:', '']);
+            setLabel(['Pick a time:', '', '']);
         } else {
             setValue(props.data.convertedTime);
-            setLabel([`When it's ${props.data.originalTime} in 
-                      ${props.data.externalTimezone}, the time here is`,
-                      `${props.data.dayResult}`]);
+            setLabel([
+                props.data.originalTime,
+                props.data.externalTimezone,
+                props.data.dayResult
+            ]);
         }
     }, [props.data])
     
@@ -22,12 +24,14 @@ function CustomTimeInput(props) {
     }
     return (
         <div className="CustomTimeInput">
-            <label htmlFor="custom-time">{label[0]}</label>
+            <label htmlFor="custom-time">
+                    When it's <span>{label[0]}</span> in <span>{label[1]}</span>, the time here is
+            </label>
             <input type="time" id="date" name="date"
                    value={value}
                    onChange={handleChange}></input>
             <div className="day-result">
-                <p>{label[1]}</p>
+                <p>{label[2]}</p>
             </div>
         </div>
     )
