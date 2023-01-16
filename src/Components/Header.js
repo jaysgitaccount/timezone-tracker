8eaa05eb90a23c8aa10125a346ba2cda2c3d7710
Jay <jay.webp@gmail.com>
2023-01-16 17:31:13 +1100

    Change header alignment depending on screen width

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,7 +11,7 @@ function Header() {
                     Have you ever found yourself thinking, <i>"I wish I knew what time it was in <span>(insert country here)</span>, so I know when I can <span>(insert time-dependent activity here)</span>"</i>? Well, now you don't have to! Using this timezone tracker, you can add and compare timezones to your heart's content.
                 </p>
                 <p>
-                    To get started, simply select a timezone from the list below.
+                    To get started, simply <span>select a timezone</span> from the list below.
                 </p>
             </div>
         </header>
